a93e0b76a0a9a81647fc4ef3c5810ca121e98fb3
Prabodhi <prabodhi38@gmail.com>
2022-09-29 23:25:09 +0200

    Routing updated

--- src/index.js
+++ src/index.js
@@ -2,33 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./components/home/Home";
 import Blogs from "./components/Blogs/Blogs";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "home",
-        element: <Home />,
-      },
-      {
-        path: "blogs",
-        element: <Blogs />,
-      },
-    ],
-  },
-]);
+function redirectToHome() {
+  console.log("triggger");
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <Router> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/" element={<Navigate to="home" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* </Router> */}
   </React.StrictMode>
 );
 
