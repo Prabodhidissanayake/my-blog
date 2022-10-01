123dc41fd41ca2ef2965ab32c784ad5a51a3e384
Prabodhi <prabodhi38@gmail.com>
2022-10-01 11:15:02 +0200

    style update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,7 +1,12 @@
 import BlogDisplay from "../blogPost/BlogDisplay";
+import "./Blogs.css"
 
 function Blogs() {
-  return <BlogDisplay count="18" showPagination="true"></BlogDisplay>;
+  return (
+    <div className="blogs">
+      <BlogDisplay count="18" showPagination="true"></BlogDisplay>
+    </div>
+  );
 }
 
 export default Blogs;
